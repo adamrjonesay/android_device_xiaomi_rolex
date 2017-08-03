d358baa192323407e14f6fc30c68eb5527bd62ab
Carlos Arriaga <carlosarriagacm@gmail.com>
2017-08-03 16:35:54 -0500

    rolex: update lcd density

--- device.mk
+++ device.mk
@@ -24,7 +24,7 @@ TARGET_SCREEN_WIDTH := 720
 
 # Screen density
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=320
+    ro.sf.lcd_density=300
 
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
